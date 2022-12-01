fbfed21dc815b055980a3541e60d78902945b41d
Julian <julu900@gmail.com>
2022-12-01 22:29:43 +0100

    add Page to minimal layout

--- app/layout/index.tsx
+++ app/layout/index.tsx
@@ -1,12 +1,17 @@
 import { type ReactNode } from "react";
 import Logo from "~/components/logo";
+import Page from '~/layout/website/page';
 
 export const MinimalLayout = ({ children }: { children: ReactNode }) => {
     return (
-        <div className="flex flex-col items-center justify-center min-h-full bg-base-50">
-            <Logo to="/"/>
-            <div className="max-w-4xl p-8 mx-auto">
-                {children}
+        <div className="flex flex-col min-h-full bg-base-50">
+            <div className="p-8 shrink">
+                <Logo to="/" />
+            </div>
+            <div className="flex flex-col items-center justify-center grow">
+                <Page>
+                    {children}
+                </Page>
             </div>
         </div>
     );
