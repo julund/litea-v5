13d3b099a8ef612cb71939479171df721c8ddfbc
Julian <julu900@gmail.com>
2022-12-01 22:30:27 +0100

    fix performance

--- app/components/pricingslider.tsx
+++ app/components/pricingslider.tsx
@@ -1,28 +1,28 @@
 import { useEffect, useState } from "react";
-import { useCounter } from "react-use";
+import { useCounter, useDebounce } from "react-use";
 import { format } from "numerable";
 import Counter from "./counter";
 
-const BASE = 10; // 8.05;
-const PPR = 0.0000195; // 0.0000195;
-const DIV = 1; // 50000000;
-const MXD = 0; // 0.30;
+const BASE = 7.50; // 8.05;
+const PPR = 0.0000250; // 0.0000195;
+const DIV = 42500000; // 50000000;
+const MXD = 0.30; // 0.30;
 
 const Pricingslider = () => {
 
     const min = 100000;
     const max = 10000000;
-    const [value, { set, reset }] = useCounter(min, max, min);
+    const [inputValue, { set, reset }] = useCounter(min, max, min);
     const [result, setResult] = useState({ price: 0, discount: 0, total: 0 });
 
-    const calculatedPrice = (value: number) => Number((BASE + (value * PPR)).toFixed(2));
-    const calculatedDiscount = (value: number) => Number(Math.min((value / DIV), MXD).toFixed(2));
+    const [value, setValue] = useState(min);
+    useDebounce(() => setValue(inputValue), 10, [inputValue]);
 
     useEffect(() => {
-        const price = calculatedPrice(value);
-        const discount = calculatedDiscount(value);
-        const total = Number((price - (price * discount)).toFixed(2));
-        setResult({ price, discount: discount, total });
+        const price = +(BASE + (value * PPR)).toFixed(5);
+        const discount = +Math.min((value / DIV), MXD).toFixed(4);
+        const total = price - (price * discount);
+        setResult({ price, discount, total });
     }, [value]);
 
     return (
@@ -30,7 +30,7 @@ const Pricingslider = () => {
             {/* <span className="text-3xl font-semibold">{format(value, "0,0")}</span> */}
             <Counter className="text-3xl font-semibold" value={value} defaultValue={min} callback={(n: number) => format(n, "0,0")}/>
             <span className="text-base-700">monthly pageviews</span>
-            <input type="range" min={min} max={max} value={value} onChange={(e) => set(Number(e.target.value))} className="slider" id="pricing"></input>
+            <input type="range" min={min} max={max} value={inputValue} step={1} onChange={(e) => set(Number(e.target.value))} className="slider" id="pricing"></input>
             <div className="flex flex-col gap-2 p-4 rounded-sm bg-base-200">
                 <div className="grid grid-cols-3 gap-2">
                     <div className="text-sm text-base-600">Base price</div>
@@ -49,13 +49,13 @@ const Pricingslider = () => {
                 </div>
             </div>
             <div className="flex flex-wrap gap-2 p-4">
-                <button className="text-sm rounded-sm text-base-600" onClick={() => set(100000)}>100k</button>
-                <button className="text-sm rounded-sm text-base-600" onClick={() => set(250000)}>250k</button>
-                <button className="text-sm rounded-sm text-base-600" onClick={() => set(500000)}>500k</button>
-                <button className="text-sm rounded-sm text-base-600" onClick={() => set(1000000)}>1 m</button>
-                <button className="text-sm rounded-sm text-base-600" onClick={() => set(5000000)}>5 m</button>
-                <button className="text-sm rounded-sm text-base-600" onClick={() => set(10000000)}>10 m</button>
-                <button className="text-sm rounded-sm text-base-600" onClick={() => reset()}>Reset</button>
+                <button className="text-sm button button-base" onClick={() => set(100000)}>100k</button>
+                <button className="text-sm button button-base" onClick={() => set(250000)}>250k</button>
+                <button className="text-sm button button-base" onClick={() => set(500000)}>500k</button>
+                <button className="text-sm button button-base" onClick={() => set(1000000)}>1 m</button>
+                <button className="text-sm button button-base" onClick={() => set(5000000)}>5 m</button>
+                <button className="text-sm button button-base" onClick={() => set(10000000)}>10 m</button>
+                <button className="text-sm button button-base" onClick={() => reset()}>Reset</button>
             </div>
         </div>
     );
