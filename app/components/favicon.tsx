1cb9b05fa39264fccd8862bcace327502cc1b5d0
Julian <julu900@gmail.com>
2022-12-01 19:01:15 +0100

    change url

--- app/components/favicon.tsx
+++ app/components/favicon.tsx
@@ -1,17 +1,16 @@
 import { isValidDomain } from "~/utils/helpers";
 import { IconQuestionMark } from "./icons";
-
+  
 export default function FavIcon({ domain, size = 16, alt, className, ...props }: { domain: string; size?: number; alt: string; className?: string; props?: React.DetailedHTMLProps<React.ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement> }) {
 
-    // `https://s2.googleusercontent.com/s2/favicons?domain_url=${domain}&sz=${size}`
-    // `https://www.google.com/s2/favicons?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&domain=${domain}&size=${size}`
-    // `https://t1.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${domain}&size=${size}`
-    // `https://t1.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&domain=${domain}&size=${size}`
-
     if (!isValidDomain(domain)) return <IconQuestionMark size={size} className={className} title={alt} />;
-    // const validUrl = validUrlDomain(domain);
+
+    const url = `https://s2.googleusercontent.com/s2/favicons?domain_url=${domain}&sz=${size}`;
+    // const url = `https://www.google.com/s2/favicons?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&domain=${domain}&size=${size}`;
+    // const url = `https://t1.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://${domain}&size=${size}`;
+
     return (
-        <img src={`https://s2.googleusercontent.com/s2/favicons?domain_url=${domain}&sz=${size}`} className={className} alt={alt} {...props} />
+        <img src={url} className={className} alt={alt} {...props} />
     );
 
 }
