a759b2b23bd951714c69d39b5312a56c7289853e
Julian <julu900@gmail.com>
2022-12-02 21:18:40 +0100

    fix responsiveness

--- app/layout/shared/nav.tsx
+++ app/layout/shared/nav.tsx
@@ -11,19 +11,23 @@ const Nav = ({ children }: { children?: React.ReactNode; }) => {
     const [expanded, toggle] = useToggle(false);
 
     return (
-        <nav ref={ref} className="flex items-start w-full gap-2 p-8 shrink">
-            <div className="flex grow">
+        <nav ref={ref} className={classNames("z-30 flex items-start w-full gap-2 p-8 shrink",
+            (!showToggle) ? "flex-row" : "flex-col"
+        )}>
+            <div className={classNames("flex grow", 
+                showToggle && "justify-between w-full ")}>
                 <Link to="/" className="inline-flex p-2 select-none">
                     <Brand />
                 </Link>
-            </div>
-            {!!children && <div className="relative top-0 right-0 flex flex-col gap-2 md:flex-row-reverse">
-                {showToggle && <button aria-label="nav-toggle" onClick={toggle} className="self-end button button-ghost">
+                {children && showToggle && <button aria-label="nav-toggle" onClick={toggle} className="justify-end button button-ghost">
                     {!expanded ? <IconMenu size={22} /> : <IconX size={22} />}
                 </button>}
+            </div>
+            {!!children && <div className="relative top-0 right-0 flex flex-col w-full gap-2 md:flex-row-reverse">
+
                 <div aria-labelledby="nav-toggle" aria-expanded={expanded} className={classNames(
-                    "relative flex flex-col md:flex-row items-start shrink gap-2 bg-base-300",
-                    !expanded && showToggle && "hidden"
+                    "relative flex items-start shrink gap-2",
+                    (!expanded && showToggle) ? "flex-row hidden" : showToggle && "flex-col p-4 w-full items-center"
                 )}>
                     {children}
                 </div>
