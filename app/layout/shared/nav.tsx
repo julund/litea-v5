8ebbdc0e11442f72e534c5a55bd4a08dbab261e0
Julian <julu900@gmail.com>
2022-12-02 22:01:41 +0100

    add close on children click

--- app/layout/shared/nav.tsx
+++ app/layout/shared/nav.tsx
@@ -3,10 +3,12 @@ import { IconMenu, IconX } from "~/components/icons";
 import Brand from "~/components/brand";
 import { classNames } from "~/utils/helpers";
 import { Link } from "~/components/link";
+import { useRef } from "react";
 
 const Nav = ({ children }: { children?: React.ReactNode; }) => {
 
     const [ref, { width }] = useMeasure<HTMLElement>();
+    const navToggleRef = useRef<HTMLDivElement>(null);
     const showToggle = width <= 704;
     const [expanded, toggle] = useToggle(false);
 
@@ -19,13 +21,12 @@ const Nav = ({ children }: { children?: React.ReactNode; }) => {
                 <Link to="/" className="inline-flex p-2 select-none">
                     <Brand />
                 </Link>
-                {children && showToggle && <button aria-label="nav-toggle" onClick={toggle} className="justify-end button button-ghost">
+                {!!children && showToggle && <button aria-label="nav-toggle" onClick={toggle} className="justify-end button button-ghost">
                     {!expanded ? <IconMenu size={22} /> : <IconX size={22} />}
                 </button>}
             </div>
             {!!children && <div className="relative top-0 right-0 flex flex-col w-full gap-2 md:flex-row-reverse">
-
-                <div aria-labelledby="nav-toggle" aria-expanded={expanded} className={classNames(
+                <div ref={navToggleRef} aria-labelledby="nav-toggle" aria-expanded={expanded} onClick={(e) => (e.target !== navToggleRef.current) && toggle() } className={classNames(
                     "relative flex items-start shrink gap-2",
                     (!expanded && showToggle) ? "flex-row hidden" : showToggle && "flex-col p-4 w-full items-center"
                 )}>
