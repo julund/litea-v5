b6ff008ea91476dd30aef99e18e136b3753dbafa
Julian <julu900@gmail.com>
2022-12-04 22:55:15 +0100

    fix bar heights

--- app/components/charts/horizontalBarChart.tsx
+++ app/components/charts/horizontalBarChart.tsx
@@ -4,26 +4,29 @@ import Spinner from "../ui/spinner";
 
 const HorizontalBarChart = ({ data }: { data: Array<any> }) => {
 
-    const sumAll = data && data?.reduce((total, item) => total + (item?.pageViews || 0), 0);
-    const dataWithPercent = data && data?.map(item => {
-        return { ...item, percent: ((item?.pageViews || 0) / sumAll), uniquePercent: ((item?.uniqueVisits || 0) / sumAll) };
-    });
-    const modifiedData = dataWithPercent;
+    // const sumAll = data && data?.reduce((total, item) => total + (item?.pageViews || 0), 0);
+    // const dataWithPercent = data && data?.map(item => {
+    //     return { ...item, percent: ((item?.pageViews || 0) / sumAll), uniquePercent: ((item?.uniqueVisits || 0) / sumAll) };
+    // });
+    // const modifiedData = dataWithPercent;
+
+    const max = data && data?.reduce((total, item) => (item?.pageViews > total) ? item?.pageViews : total, 0) * 1.25;
+    const dataValues = data && data?.map(item => {
+            return { ...item, percent: ((item?.pageViews || 0) / max), uniquePercent: ((item?.uniqueVisits || 0) / max) };
+        });
+    const modifiedData = dataValues;
 
     return (
         <div className="h-40 rounded-sm select-none bg-base-50">
             <div className="flex flex-row justify-center w-full h-full gap-1 p-4">
                 {!modifiedData && <div className="flex items-center"> <Spinner size={64} /> </div>}
                 {modifiedData && modifiedData.map((item, i) => {
-
                     const len = modifiedData.length;
                     const hide = (len === 7 || len === 12 || len === 11) ? false : len > 30 ? i % 4 !== 0 : i % 2 !== 0;
-
-                    // if (!item.pageViews) return(<div key={i} className="flex-grow"></div>);
                     return (
                         <div key={i} className="flex-grow bar">
                             {item.pageViews > 0 || item.pageViews === 0 ?
-                                <div className="h-full cursor-pointer opacity-80 hover:opacity-100">
+                                <div className="h-full cursor-pointer opacity-80 hover:opacity-100" title={`Page views: ${item.pageViews}`}>
                                     <Bar value={item.percent} inPercent={true} horizontal={false} />
                                     <div className="flex justify-center py-2 text-xs font-semibold text-gray-800">
                                         {!hide && item.label}
