c89cbff07b03666330de63428b173019a2490bd0
Julian <julu900@gmail.com>
2022-12-07 16:29:13 +0100

    add icon

--- app/components/icons.tsx
+++ app/components/icons.tsx
@@ -25,6 +25,7 @@ export const IconWorld = ({ ...props }: IconProps) => icon.TbWorld({ ...defaultI
 export const IconUser = ({ ...props }: IconProps) => icon.TbUser({ ...defaultIconProps, ...props });
 export const IconChevronDown = ({ ...props }: IconProps) => icon.TbChevronDown({ ...defaultIconProps, ...props });
 export const IconChevronUp = ({ ...props }: IconProps) => icon.TbChevronUp({ ...defaultIconProps, ...props });
+export const IconChevronsUpRight = ({ ...props }: IconProps) => icon.TbChevronsUpRight({ ...defaultIconProps, ...props });
 export const IconChevronRight = ({ ...props }: IconProps) => icon.TbChevronRight({ ...defaultIconProps, ...props });
 export const IconChevronLeft = ({ ...props }: IconProps) => icon.TbChevronLeft({ ...defaultIconProps, ...props });
 export const IconCircle = ({ ...props }: IconProps) => icon.TbCircle({ ...defaultIconProps, ...props });
