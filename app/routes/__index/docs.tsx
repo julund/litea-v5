0f48df0e619e8fe607a5e19cf12542d00da98ba8
julund <julu900@gmail.com>
2022-12-12 10:28:09 +0000

    add text

---
+++ app/routes/__index/docs.tsx
@@ -0,0 +1,63 @@
+import Container from "~/layout/shared/container";
+
+export default function DocsPage() {
+    return (
+        <Container>
+            <h1>Docs</h1>
+            <p className="py-4">
+                Autem esse quia quis et quia. Qui est facilis consequuntur
+                dolorem. Ipsam et magnam est quia ut et beatae. Cum dolores veritatis
+                ut quaerat ut quo eligendi voluptas.
+            </p>
+            <h2>Frequently asked questions</h2>
+            <p className="py-4">
+                Q: What is Litea?
+                A: Litea is a website analytics application that helps website owners and administrators understand how their site is being used and accessed by visitors.
+            </p>
+            <p className="py-4">
+                Q: How does Litea differ from other website analytics tools?
+                A: Litea is a lightweight tool that is free from cookies and fully compliant with GDPR, CCPA and PECR. It also includes a responsive dashboard that makes it easy for users to quickly analyze and understand their website's performance.
+            </p>
+            <p className="py-4">
+                Q: What information can I track with Litea?
+                A: With Litea, you can track a wide range of metrics, including the number of visitors to your site, the pages that are most frequently visited, the amount of time that users spend on your site, and the specific actions that users take while on your site. You can also see detailed information about the demographics of your audience, including their age, gender, and geographic location.
+            </p>
+            <p className="py-4">
+                Q: Can I use Litea to track an unlimited number of sites?
+                A: Yes, Litea allows you to track an unlimited number of sites.
+            </p>
+            <p className="py-4">
+                Q: Does Litea provide data ownership?
+                A: Yes, with Litea you have 100% ownership of your data.
+            </p>
+            <p className="py-4">
+                Q: Does Litea include uptime monitoring?
+                A: Yes, Litea includes uptime monitoring to ensure that your websites are always available.
+            </p>
+            <p className="py-4">
+                Q: Does Litea require cookie banners?
+                A: No, Litea does not require any cookie banners.
+            </p>
+            <p className="py-4">
+                Q: Can I receive email reports with Litea?
+                A: Yes, Litea allows you to receive email reports with your website's performance data.
+            </p>
+            <p className="py-4">
+                Q: Does Litea provide forever data retention?
+                A: Yes, Litea offers forever data retention for your website's performance data.
+            </p>
+            <p className="py-4">
+                Q: Can I export data from Litea in CSV or JSON format?
+                A: Yes, Litea allows you to export your website's performance data in CSV or JSON format, and there is no limit to the number of exports you can perform.
+            </p>
+            <p className="py-4">
+                Q: Is Litea compliant with privacy laws?
+                A: Yes, Litea is compliant with GDPR, CCPA and PECR.
+            </p>
+            <p className="py-4">
+                Q: What is the infrastructure like for Litea?
+                A: Litea is built on enterprise-grade infrastructure, ensuring that it is reliable and can handle large amounts of data.
+            </p>
+        </Container>
+    );
+};
--- app/routes/__index/index.tsx
+++ app/routes/__index/index.tsx
@@ -21,6 +21,13 @@ export default function Home() {
                         <a className="button button-ghost" href={"/"}>Live demo</a>
                     </div>
                 </div>
+                <p className="py-2">
+                    Litea Analytics is a website analytics application that helps website owners and administrators understand how their site is being used and accessed by visitors. This application allows users to track a wide range of metrics for an unlimited number of sites, giving them 100% ownership of their data.
+                </p>
+                <p className="py-2">
+                    The application also includes uptime monitoring to ensure that users' websites are always available, and does not require any cookie banners. Users can receive email reports and have access to forever data retention, as well as unlimited CSV and JSON data exports. Additionally, Litea Analytics is compliant with privacy laws and is built on enterprise-grade infrastructure.
+                </p>
+
             </div>
         </Container>
     );
