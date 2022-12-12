73b1b5dda6c5fe48295e594e8a0471462bc6c346
Julian <julu900@gmail.com>
2022-12-12 20:03:56 +0100

    add FAQ

--- app/routes/__index/docs.tsx
+++ app/routes/__index/docs.tsx
@@ -1,5 +1,32 @@
 import Container from "~/layout/shared/container";
 
+const questionsAndAnswers = [
+    { question: "What is Litea?", answer: "Litea is a website analytics application that helps website owners and administrators understand how their site is being used and accessed by visitors." },
+    { question: "How does Litea differ from other website analytics tools?", answer: "Litea is a lightweight tool that is free from cookies and fully compliant with GDPR, CCPA and PECR. It also includes a responsive dashboard that makes it easy for users to quickly analyze and understand their website's performance." },
+    { question: "What information can I track with Litea?", answer: "With Litea, you can track a wide range of metrics, including the number of visitors to your site, the pages that are most frequently visited, the amount of time that users spend on your site, and the specific actions that users take while on your site. You can also see detailed information about the demographics of your audience, including their age, gender, and geographic location." },
+    { question: "How do I get started with Litea?", answer: "To get started with Litea, you'll need to create an account. Once you've created an account, you can add your website to Litea and start tracking your site's performance." },
+    { question: "Can I use Litea to track an unlimited number of sites?", answer: "Yes, Litea allows you to track an unlimited number of sites." },
+    { question: "Does Litea provide data ownership?", answer: "Yes, with Litea you have 100% ownership of your data." },
+    { question: "Does Litea include uptime monitoring?", answer: "Yes, Litea includes uptime monitoring to ensure that your websites are always available." },
+    { question: "Does Litea require cookie banners?", answer: "No, Litea does not require any cookie banners." },
+    { question: "Can I receive email reports with Litea?", answer: "Yes, Litea allows you to receive email reports with your website's performance data." },
+    { question: "Does Litea provide forever data retention?", answer: "Yes, Litea offers forever data retention for your website's performance data." },
+    { question: "Can I export data from Litea in CSV or JSON format?", answer: "Yes, Litea allows you to export your website's performance data in CSV or JSON format, and there is no limit to the number of exports you can perform." },
+    { question: "Is Litea compliant with privacy laws?", answer: "Yes, Litea is compliant with GDPR, CCPA and PECR." },
+    { question: "What is the infrastructure like for Litea?", answer: "Litea is built on enterprise-grade infrastructure, ensuring that it is reliable and can handle large amounts of data." },
+];
+
+const QAitem = ({ question, answer } : { question: string; answer: string }) => (
+    <details className="flex gap-2 rounded-sm px-4 py-2 open:bg-base-100 text-base-800 group transition-all duration-500">
+        <summary className="py-2 select-none cursor-pointer flex justify-center items-center">
+            <span className="font-semibold grow">{question}</span>
+            <span className="text-lg hidden group-open:inline">-</span>
+            <span className="text-lg inline group-open:hidden">+</span>
+        </summary>
+        <p className="py-4">{answer}</p>
+    </details>
+);
+
 export default function DocsPage() {
     return (
         <Container>
@@ -9,55 +36,12 @@ export default function DocsPage() {
                 dolorem. Ipsam et magnam est quia ut et beatae. Cum dolores veritatis
                 ut quaerat ut quo eligendi voluptas.
             </p>
-            <h2>Frequently asked questions</h2>
-            <p className="py-4">
-                Q: What is Litea?
-                A: Litea is a website analytics application that helps website owners and administrators understand how their site is being used and accessed by visitors.
-            </p>
-            <p className="py-4">
-                Q: How does Litea differ from other website analytics tools?
-                A: Litea is a lightweight tool that is free from cookies and fully compliant with GDPR, CCPA and PECR. It also includes a responsive dashboard that makes it easy for users to quickly analyze and understand their website's performance.
-            </p>
-            <p className="py-4">
-                Q: What information can I track with Litea?
-                A: With Litea, you can track a wide range of metrics, including the number of visitors to your site, the pages that are most frequently visited, the amount of time that users spend on your site, and the specific actions that users take while on your site. You can also see detailed information about the demographics of your audience, including their age, gender, and geographic location.
-            </p>
-            <p className="py-4">
-                Q: Can I use Litea to track an unlimited number of sites?
-                A: Yes, Litea allows you to track an unlimited number of sites.
-            </p>
-            <p className="py-4">
-                Q: Does Litea provide data ownership?
-                A: Yes, with Litea you have 100% ownership of your data.
-            </p>
-            <p className="py-4">
-                Q: Does Litea include uptime monitoring?
-                A: Yes, Litea includes uptime monitoring to ensure that your websites are always available.
-            </p>
-            <p className="py-4">
-                Q: Does Litea require cookie banners?
-                A: No, Litea does not require any cookie banners.
-            </p>
-            <p className="py-4">
-                Q: Can I receive email reports with Litea?
-                A: Yes, Litea allows you to receive email reports with your website's performance data.
-            </p>
-            <p className="py-4">
-                Q: Does Litea provide forever data retention?
-                A: Yes, Litea offers forever data retention for your website's performance data.
-            </p>
-            <p className="py-4">
-                Q: Can I export data from Litea in CSV or JSON format?
-                A: Yes, Litea allows you to export your website's performance data in CSV or JSON format, and there is no limit to the number of exports you can perform.
-            </p>
-            <p className="py-4">
-                Q: Is Litea compliant with privacy laws?
-                A: Yes, Litea is compliant with GDPR, CCPA and PECR.
-            </p>
-            <p className="py-4">
-                Q: What is the infrastructure like for Litea?
-                A: Litea is built on enterprise-grade infrastructure, ensuring that it is reliable and can handle large amounts of data.
-            </p>
+            <div className="divide-y">
+                <h2>Frequently asked questions</h2>
+                {questionsAndAnswers.map((qa) => (
+                    <QAitem key={qa.question} {...qa} />
+                ))}
+            </div>
         </Container>
     );
 };
